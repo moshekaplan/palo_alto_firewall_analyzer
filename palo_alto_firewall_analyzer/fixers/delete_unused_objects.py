db1f36cbe432ac1fb0bcea8cc9387a7abfa08259
Moshe Kaplan <me@moshekaplan.com>
2022-02-15 08:26:43 -0500

    Delete Unused Objects: pep8 cleanups

--- palo_alto_firewall_analyzer/fixers/delete_unused_objects.py
+++ palo_alto_firewall_analyzer/fixers/delete_unused_objects.py
@@ -1,8 +1,8 @@
 from palo_alto_firewall_analyzer.core import register_policy_fixer, get_policy_validators
-
 from palo_alto_firewall_analyzer import pan_api
 
 import requests
+
 
 def delete_unused_object(profilepackage, object_type, object_friendly_type, validator_function):
     panorama = profilepackage.settings.get("Panorama")
@@ -10,26 +10,28 @@ def delete_unused_object(profilepackage, object_type, object_friendly_type, vali
     pan_config = profilepackage.pan_config
     version = pan_config.get_major_version()
 
-    print ("*"*80)
-    print (f"Checking for unused {object_friendly_type} objects to delete")
+    print("*" * 80)
+    print(f"Checking for unused {object_friendly_type} objects to delete")
 
     results_to_delete = validator_function(profilepackage)
 
-    if results_to_delete:
-        print (f"Deleting {len(results_to_delete)} unused {object_friendly_type} objects now")
-        for badentry in results_to_delete:
-            device_group = badentry.device_group
-            object_name = badentry.data[0].get('name')
-            print(f"Deleting {object_name} from {device_group}")
-            try:
-                pan_api.delete_object(panorama, version, api_key, object_type, object_name, device_group)
-            except requests.HTTPError as err:
-                print(f"Error deleting {object_name} from {device_group}: {err.response.text}")
-        pan_api.validate_commit(panorama, api_key)
-        print (f"Deletion of {len(results_to_delete)} unused {object_friendly_type} objects complete. Please commit in the firewall.")
-    else:
-        print (f"There were no {object_friendly_type} to delete")
+    if not results_to_delete:
+        print(f"There were no {object_friendly_type} to delete")
+        return results_to_delete
+
+    print(f"Deleting {len(results_to_delete)} unused {object_friendly_type} objects now")
+    for badentry in results_to_delete:
+        device_group = badentry.device_group
+        object_name = badentry.data[0].get('name')
+        print(f"Deleting {object_name} from {device_group}")
+        try:
+            pan_api.delete_object(panorama, version, api_key, object_type, object_name, device_group)
+        except requests.HTTPError as err:
+            print(f"Error deleting {object_name} from {device_group}: {err.response.text}")
+    pan_api.validate_commit(panorama, api_key)
+    print(f"Deletion of {len(results_to_delete)} unused {object_friendly_type} objects complete. Please commit in the firewall.")
     return results_to_delete
+
 
 @register_policy_fixer("DeleteUnusedAddresses", "Delete Address objects that aren't in use")
 def delete_unused_addresses(profilepackage):
@@ -38,12 +40,14 @@ def delete_unused_addresses(profilepackage):
     _, _, validator_function = get_policy_validators()['UnusedAddresses']
     return delete_unused_object(profilepackage, object_type, object_friendly_type, validator_function)
 
+
 @register_policy_fixer("DeleteUnusedAddressGroups", "Delete AddressGroup objects that aren't in use")
 def delete_unused_addressgroups(profilepackage):
     object_type = "AddressGroups"
     object_friendly_type = "Address Group"
     _, _, validator_function = get_policy_validators()['UnusedAddressGroups']
     return delete_unused_object(profilepackage, object_type, object_friendly_type, validator_function)
+
 
 @register_policy_fixer("DeleteUnusedServices", "Delete Service objects that aren't in use")
 def delete_unused_services(profilepackage):
@@ -52,12 +56,14 @@ def delete_unused_services(profilepackage):
     _, _, validator_function = get_policy_validators()['UnusedServices']
     return delete_unused_object(profilepackage, object_type, object_friendly_type, validator_function)
 
+
 @register_policy_fixer("DeleteUnusedServiceGroups", "Delete Service Group objects that aren't in use")
 def delete_unused_servicegroups(profilepackage):
     object_type = "ServiceGroups"
     object_friendly_type = "Service Groups"
     _, _, validator_function = get_policy_validators()['UnusedServiceGroups']
     return delete_unused_object(profilepackage, object_type, object_friendly_type, validator_function)
+
 
 @register_policy_fixer("DeleteUnusedOjects", "Convenience wrapper that calls DeleteUnusedAddressGroups, DeleteUnusedAddresses, DeleteUnusedServiceGroups, and DeleteUnusedServices")
 def delete_unused_objects(profilepackage):
