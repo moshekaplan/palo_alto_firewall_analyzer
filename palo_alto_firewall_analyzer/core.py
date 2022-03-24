2ad26cfec77950f9f004aecb0fd4c569743fbb94
Moshe Kaplan <me@moshekaplan.com>
2022-03-24 16:12:04 -0400

    Add missing parentheses

--- palo_alto_firewall_analyzer/core.py
+++ palo_alto_firewall_analyzer/core.py
@@ -79,13 +79,13 @@ class ConfigurationSettings:
             self.local_config.set('Analyzer', '# Specify which Security Profile Groups are allowed and the default profile')
             self.local_config.set('Analyzer', '# Allowed Group Profiles = Security Profile Group-default,Security Profile Group-1,Security Profile Group-2')
             self.local_config.set('Analyzer', '# Default Group Profile = Security Profile Group-default')
-            self.local_config.set('Analyzer', '# UnconventionallyNamedAddresses: Specify a format for Address object names. Available fields are: {host}, {network}, {range}, {fqdn}, {mask}'
-            self.local_config.set('Analyzer', '# host is for an IPv4 with a /32 netmask, IPv6 with a /128, or a host without a netmask at all'
-            self.local_config.set('Analyzer', 'fqdn name format = fqdn-{fqdn}'
-            self.local_config.set('Analyzer', 'host name format = host-{host}'
-            self.local_config.set('Analyzer', 'net name format = net-{host}_{network}'
-            self.local_config.set('Analyzer', 'range name format = range-{range}'
-            self.local_config.set('Analyzer', 'wildcard name format = wildcard-{mask}'
+            self.local_config.set('Analyzer', '# UnconventionallyNamedAddresses: Specify a format for Address object names. Available fields are: {host}, {network}, {range}, {fqdn}, {mask}')
+            self.local_config.set('Analyzer', '# host is for an IPv4 with a /32 netmask, IPv6 with a /128, or a host without a netmask at all')
+            self.local_config.set('Analyzer', 'fqdn name format = fqdn-{fqdn}')
+            self.local_config.set('Analyzer', 'host name format = host-{host}')
+            self.local_config.set('Analyzer', 'net name format = net-{host}_{network}')
+            self.local_config.set('Analyzer', 'range name format = range-{range}')
+            self.local_config.set('Analyzer', 'wildcard name format = wildcard-{mask}')
             self.local_config.set('Analyzer', '# UnconventionallyNamedServices: Specify a format for Service object names. Available fields are: {transport}, {source-port}, {port}, {override}')
             self.local_config.set('Analyzer', '# service name format = {transport}-{port}')
             self.local_config.set('Analyzer', '# EquivalentObjects: Whether to ignore the description field when comparing if two objects are equivalent (false by default)')
