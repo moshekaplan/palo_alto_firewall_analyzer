7369fe9d13ce1eb3f55c4fbff651375bc24e2078
Moshe Kaplan <me@moshekaplan.com>
2023-03-06 12:52:46 -0500

    Add scripts directory with one-off scripts based on the API tools

---
+++ scripts/disable_rules.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# Script for disabling a list of rules from a given device group
+# This is intended to aid cleaning up unused rules
+# discovered with Policy Optimizer
+
+import argparse
+import os
+
+
+from palo_alto_firewall_analyzer import pan_api
+from palo_alto_firewall_analyzer.core import xml_object_to_dict
+from palo_alto_firewall_analyzer.pan_config import PanConfig
+from palo_alto_firewall_analyzer.pan_helpers import get_and_save_API_key
+
+
+DEFAULT_CONFIG_DIR = os.path.expanduser("~\\.pan_policy_analyzer\\")
+DEFAULT_API_KEYFILE = DEFAULT_CONFIG_DIR + "API_KEY.txt"
+
+
+def load_api_key(api_file):
+    try:
+        with open(api_file) as fh:
+            api_key = fh.read().strip()
+    except OSError:
+        print(f"Unable to open file with API key '{api_file}'")
+        api_key = get_and_save_API_key(api_file)
+    return api_key
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Disable a list of security rules")
+    parser.add_argument("panorama", nargs=1, help="Panorama to run on")
+    parser.add_argument("device-group", nargs=1, help="Device Group to disable rules from")
+    parser.add_argument("policy_type", nargs=1, help="Rule type", choices=['SecurityPreRules', 'SecurityPostRules'])
+    parser.add_argument("infile", nargs=1, help="Input file with list of rules to disable")
+    parser.add_argument("--api", help=f"File with API Key (default is {DEFAULT_API_KEYFILE})", default=DEFAULT_API_KEYFILE)
+
+    parsed_args = parser.parse_args()
+    try:
+        with open(parsed_args.api) as fh:
+            api_key = fh.read().strip()
+    except OSError:
+        print(f"Unable to open file with API key '{parsed_args.api}'")
+        api_key = get_and_save_API_key(parsed_args.api)
+
+    with open(parsed_args.infile[0]) as fh:
+        rules_to_disable = set(fh.read().splitlines())
+
+    panorama = parsed_args.panorama[0]
+    device_group = vars(parsed_args)['device-group'][0]
+    policy_type = parsed_args.policy_type[0]
+
+    xml_config = pan_api.export_configuration2(panorama, api_key)
+    pan_config = PanConfig(xml_config)
+
+    version = pan_config.get_major_version()
+
+    for rule_entry in pan_config.get_devicegroup_policy(policy_type, device_group):
+        rule_name = rule_entry.get('name')
+        if rule_name in rules_to_disable:
+            disabled = (rule_entry.find('disabled') is not None and rule_entry.find('disabled').text == 'yes')
+            if disabled:
+                print(f"Rule {rule_entry} was already disabled, no action needed!")
+            else:
+                policy_dict = xml_object_to_dict(rule_entry)['entry']
+                policy_dict['disabled'] = 'yes'
+                print(f"Disabling {device_group}'s {policy_type} {rule_name}")
+                pan_api.update_devicegroup_policy(panorama, version, api_key, policy_dict, policy_type, device_group)
+
+
+if __name__ == '__main__':
+    main()
